78127894e59116a4f5b1a39dd156b8b940b59d25
tarotgamerz <ravikumarbharti611@gmail.com>
2026-09-25 21:56:35 +0530

    Fix Operator page ESM imports

--- operator/app/page.tsx
+++ operator/app/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
-import { hasOperatorSession } from "../lib/private-auth";
-import { OperatorConsole } from "./operator-console";
+import { hasOperatorSession } from "../lib/private-auth.js";
+import { OperatorConsole } from "./operator-console.js";
 
 export default async function Page() {
   if (!(await hasOperatorSession())) {
